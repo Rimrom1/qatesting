e85c634debbb58fcc108370b445431ef9f22d09d
Rimrom1 <8000zoro@gmail.com>
2022-02-11 17:08:25 +0200

    Робота над калькулятором і стрінговим калькуляторм

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 ##Task by Roman Borak 
+## https://docs.google.com/spreadsheets/d/1_dB4QUfhx_-qdgBZBgRxWWczBLeAGoq6Zvjok74NjP0/edit#gid=0 - таблиця
--- src/main/java/file0/Calculator.java
+++ src/main/java/file0/Calculator.java
@@ -1,4 +1,31 @@
 package file0;
 
 public class Calculator {
+    public static void main(String[] args){
+        int a = 5;
+        int b = 6;
+
+        System.out.println(plus(a,b));
+        System.out.println(minus(a,b));
+        System.out.println(divide(a,b));
+        System.out.println(multiplying(a,b));
+    }
+
+    private static int plus(int a, int b) {
+
+        return a + b;
+    }
+    private static int minus(int a, int b) {
+
+
+        return a - b;
+    }
+    private static int divide(int a, int b) {
+
+        return a / b;
+    }
+    private static int multiplying(int a, int b) {
+
+        return a * b;
+    }
 }
